7432134cb008c16cacf544322309eed5c6c42fb1
DavidTippy <davidtippy@gmail.com>
2020-02-24 10:55:31 -0600

    Added basic jump physics
    followed Sebastian's tutorial through episode 3

--- Project Acorn Prototype 1/Assets/Scripts/Controller2D.cs
+++ Project Acorn Prototype 1/Assets/Scripts/Controller2D.cs
@@ -4,13 +4,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//make sure that the player has a BoxCollider2D component
 [RequireComponent (typeof (BoxCollider2D))]
 public class Controller2D : MonoBehaviour
 {
 
+    //create collider and raycastOrigins struct (collider corners)
     BoxCollider2D collider;
     RaycastOrigins raycastOrigins;
 
+    public CollisionInfo collisions;
+
+    //distance to inset the rays by
     const float skinWidth = .015f;
 
     //number of rays
@@ -20,6 +25,9 @@ public class Controller2D : MonoBehaviour
     //spacing of the rays
     float horizontalRaySpacing;
     float verticalRaySpacing;
+
+    //layer to collide with
+    public LayerMask collisionMask;
 
     // Start is called before the first frame update
     void Start()
@@ -31,19 +39,113 @@ public class Controller2D : MonoBehaviour
 
     public void Move(Vector2 velocity)
     {
-
+        //make sure we know where the corners of the collider are
         UpdateRaycastOrigins();
 
+        //reset the collision info
+        collisions.Reset();
 
+        //figure out if we're going to run into something to our left or right
+        if (velocity.x != 0)
+        {
+            HorizontalCollisions(ref velocity);
+        }
+
+        //figure out if we've hit something above or below us
+        if (velocity.y != 0)
+        {
+            VerticalCollisions(ref velocity);
+        }
         //This line moves the player; everything before this line in this method is to figure 
         //out what the velocity should actually be.
         transform.Translate(velocity);
     }
 
-    void VerticalCollisions()
+    void HorizontalCollisions(ref Vector2 velocity)
     {
+        //determine if we are traveling left or right
+        float directionX = Mathf.Sign(velocity.x);
 
+        //determine how long the ray needs to be so it can "see" everything we could possibly hit
+        float rayLength = Mathf.Abs(velocity.x) + skinWidth;
+
+        //loop throught horizontal rays to see if we hit something
+        for (int i = 0; i < horizontalRayCount; i++)
+        {
+
+            //use the top rayOrigin if we are moving up, use the bottom rayOrigin if we are moving down
+            Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+            //calculate where to put the next ray
+            rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+
+            //check for hit
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+
+            //Draw rays
+            Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
+
+            if (hit)
+            {
+                //change velocity to avoid going inside wall
+                velocity.x = (hit.distance - skinWidth) * directionX;
+
+                //Change ray length if we hit something so we can't hit something further away 
+                rayLength = hit.distance;
+
+                //update collision info
+                collisions.left = directionX == -1;
+                collisions.right = directionX == 1;
+
+            }//end if
+
+        }//end for
     }
+
+    void VerticalCollisions(ref Vector2 velocity)
+    {
+        //determine if we are traveling up or down
+        float directionY = Mathf.Sign(velocity.y);
+
+        //determine how long the ray needs to be so it can "see" everything we could possibly hit
+        float rayLength = Mathf.Abs(velocity.y) + skinWidth;
+
+        //loop throught vertical rays to see if we hit something
+        for (int i = 0; i < verticalRayCount; i++)
+        {
+
+            //use the top rayOrigin if we are moving up, use the bottom rayOrigin if we are moving down
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+            //calculate where to put the next ray
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+
+            //check for hit
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+
+            //Draw rays
+            Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.red);
+
+            if(hit)
+            {
+                //change velocity to avoid going inside wall
+                velocity.y = (hit.distance - skinWidth) * directionY;
+
+                //Change ray length if we hit something so we can't hit something further away 
+                rayLength = hit.distance;
+
+                //update collision info
+                collisions.below = directionY == -1;
+                collisions.above = directionY == 1;
+
+
+
+
+            }//end if
+
+
+        }//end for
+    }
+
+    
 
     void UpdateRaycastOrigins()
     {
@@ -71,35 +173,31 @@ public class Controller2D : MonoBehaviour
         //calculate spacing of rays
         horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
         verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
-
     }
 
 
+    //struct that holds the corners of the collider
     struct RaycastOrigins {
 
         public Vector2 topLeft, topRight;
         public Vector2 bottomLeft, bottomRight;
-    
+  
     
     }
 
+    public struct CollisionInfo {
 
-    // Update is called once per frame
-    void Update()
-    {
+        public bool above, below;
+        public bool left, right;
 
-        
-        
-
-        
-        for (int i = 0; i < verticalRayCount; i++)
+        public void Reset()
         {
-
-            Debug.DrawRay(raycastOrigins.bottomLeft + Vector2.right * verticalRaySpacing * i, Vector2.up * -2, Color.red);
-
-
+            above = below = false;
+            left = right = false;
         }
-
-
+    
     }
+
+
+
 }
--- Project Acorn Prototype 1/Assets/Scripts/Player.cs
+++ Project Acorn Prototype 1/Assets/Scripts/Player.cs
@@ -7,22 +7,68 @@ using UnityEngine;
 [RequireComponent (typeof (Controller2D))]
 public class Player : MonoBehaviour
 {
-    float gravity = -6.0f;
+
+    public float jumpHeight = 4;
+    public float timeToJumpApex = .3f;
+
+    float accelerationTimeAirborne = .3f;
+    float accelerationTimeGrounded = .1f;
+
+    float moveSpeed = 8;
+    float gravity;
+    float jumpVelocity;
+
     Vector2 velocity;
 
+    float velocityXSmoothing;
 
+    //create Controller2D script
     Controller2D controller;
 
     // Start is called before the first frame update
     void Start()
     {
+        //get Controller2D attached to player object
         controller = GetComponent<Controller2D>();
+
+        //calculate the gravity from the jumpHeight and timeToJumpApex
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        //calculate the jumpVelocity
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+
+        //print gravity and jumpVelocity
+        //print("Gravity: " + gravity + "Jump Velocity: " + jumpVelocity);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        //reset player's vertical momentum if they hit a floor or ceiling
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            velocity.y = 0;
+        }
+
+        //get player's input
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        //get jump input
+        if(Input.GetKeyDown(KeyCode.Space) && controller.collisions.below)
+        {
+
+            velocity.y = jumpVelocity;
+
+        }
+
+        //apply player's horizontal input smoothly
+        float targetVelocityX =  input.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
+
+        //apply gravity
         velocity.y += gravity * Time.deltaTime;
+
+        //call controller's move method. this method will calculate how much to move the player to avoid collisions
         controller.Move(velocity * Time.deltaTime);
     }
 }
