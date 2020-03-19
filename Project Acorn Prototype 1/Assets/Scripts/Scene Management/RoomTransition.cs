aef0e7febe726dca26be4a6a9341bc2e159106c7
DavidTippy <davidtippy@gmail.com>
2020-03-19 18:10:28 -0500

    Started work on moving from room to room

--- Project Acorn Prototype 1/Assets/Scripts/Scene Management/RoomTransition.cs
+++ Project Acorn Prototype 1/Assets/Scripts/Scene Management/RoomTransition.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RoomTransition : MonoBehaviour
 {
+    public string roomName;
+    public float xCoordinate;
+    public float yCoordinate;
+
+    private SceneController sc;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sc = GameObject.Find("MasterGameController").GetComponent<SceneController>();
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //other.transform.position = new Vector2(xCoordinate, yCoordinate);
+            sc.ChangeRooms(roomName, xCoordinate, yCoordinate);
+        }
+
     }
 }
--- Project Acorn Prototype 1/Assets/Scripts/Scene Management/SceneController.cs
+++ Project Acorn Prototype 1/Assets/Scripts/Scene Management/SceneController.cs
@@ -1,1 +1,51 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    //private PlayerManager pm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //pm = GameObject.Find("MasterGameController").GetComponent<PlayerManager>();
+        StartCoroutine(LoadingLevel("Scene1", 0, 100));
+    }
+
+    //SaveSystem.Load().roomName
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeRooms(string roomName, float xCoord, float yCoord)
+    {
+        StartCoroutine(LoadingLevel(roomName, xCoord, yCoord));
+
+        //pm.InstantiatePlayer(xCoord, yCoord);
+
+    }
+
+    public IEnumerator LoadingLevel(string roomName, float xCoord, float yCoord)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(roomName);
+
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+            Debug.Log("room has not yet fully loaded");
+        }
+        Debug.Log("room has fully loaded");
+        Instantiate(playerPrefab, new Vector3(xCoord, yCoord, 0), Quaternion.identity);
+    }
+
+    public void SaveGame(string RoomName)
+    {
+
+    }
+}
