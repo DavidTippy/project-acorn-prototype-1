7330f96d29d02f983717accdef870e29f208c2d7
DavidTippy <davidtippy@gmail.com>
2020-03-21 13:45:00 -0500

    Added comments to camera follow script

--- Project Acorn Prototype 1/Assets/Scripts/CameraFollow.cs
+++ Project Acorn Prototype 1/Assets/Scripts/CameraFollow.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
 
-    public Controller2D target;
+    Controller2D target;
     public float verticalOffset;
     public float lookAheadDstX;
     public float lookSmoothTimeX;
@@ -32,13 +32,17 @@ public class CameraFollow : MonoBehaviour
     void LateUpdate()
     {
 
+        //if we have a player for the camera to target
         if (target ?? false)
         {
 
+            //update the focus area's position.
             focusArea.Update(target.collider.bounds);
 
+            //set the position for the camera to center on
             Vector2 focusPosition = focusArea.center + Vector2.up * verticalOffset;
 
+            //look ahead
             if (focusArea.velocity.x != 0)
             {
                 lookAheadDirX = Mathf.Sign(focusArea.velocity.x);
@@ -59,18 +63,25 @@ public class CameraFollow : MonoBehaviour
                 }
 
             }//end if
-
             targetLookAheadX = lookAheadDirX * lookAheadDstX;
             currentLookAheadX = Mathf.SmoothDamp(currentLookAheadX, targetLookAheadX, ref smoothLookVelocityX, lookSmoothTimeX);
 
-            focusPosition.y = Mathf.SmoothDamp(transform.position.y, focusPosition.y, ref smoothVelocityY, verticalSmoothTime);
+            //this line would smooth the Y axis movement - we don't want that for now.
+            //focusPosition.y = Mathf.SmoothDamp(transform.position.y, focusPosition.y, ref smoothVelocityY, verticalSmoothTime);
+
+            //change the focus position based on the current look ahead
             focusPosition += Vector2.right * currentLookAheadX;
+
+            //TODO: Check that the focus position is not out of bounds and move it back inside bounds if it is.
+
+            //move the camera, and make sure the camera is in front of the level (on the z-axis)
             transform.position = (Vector3)focusPosition + Vector3.forward * -10;
 
         }
-        else
+        else // if we do not have a player for the camera to target
         {
 
+            //find the player
             target = GameObject.FindWithTag("Player").GetComponent<Controller2D>();
             focusArea = new FocusArea(target.collider.bounds, focusAreaSize);
 
@@ -85,7 +96,7 @@ public class CameraFollow : MonoBehaviour
     }
 
 
-
+    //the focus area is a small area around the player where we can move without the camera moving.
     struct FocusArea
     {
 
@@ -138,7 +149,6 @@ public class CameraFollow : MonoBehaviour
 
             //update center
             center = new Vector2((left + right) / 2, (top + bottom) / 2);
-
 
             velocity = new Vector2(shiftX, shiftY);
 
